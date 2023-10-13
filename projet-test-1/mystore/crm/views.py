fc76792e0f0ae0d889b8d49bdddb9f563eab84e0
Adonis Rebano <earebano01@monccnb.ca>
2023-10-13 13:31:52 -0300

    créé un template et modifier l'url et route

--- projet-test-1/mystore/crm/views.py
+++ projet-test-1/mystore/crm/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return render(request, 'crm/dashboard.html')
+
+def products(request):
+    return render(request, 'crm/products.html')
+
+def customers(request):
+    return render(request, 'crm/customers.html')
